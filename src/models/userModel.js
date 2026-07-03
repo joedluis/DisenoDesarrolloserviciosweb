8a5a74675c1d61c0790ddd8d7e88600cd4a105bb
joedluis <joedluis@gmail.com>
2026-07-02 20:04:58 -0500

    feat: crear modelo de usuarios

---
+++ src/models/userModel.js
@@ -0,0 +1,11 @@
+/**
+ * Modelo de usuarios.
+ * En este proyecto almacenaremos los usuarios en memoria
+ * mediante un arreglo. No utilizaremos base de datos.
+ */
+
+// Arreglo que almacenará los usuarios registrados
+const users = [];
+
+// Exporta el arreglo para utilizarlo en otras partes del proyecto
+module.exports = users;
